33c80fe48ef3bb74ed9d94c725be025fb125e312
take72k <hyes941073@gmail.com>
2021-04-29 00:35:54 +0800

    '1/3'

---
+++ HW2/DMA.cpp
@@ -0,0 +1,106 @@
+#include "DMA.h"
+// create by take72k e24063058
+
+
+
+void DMA::b_transport(tlm::tlm_generic_payload& trans, sc_time& delay){
+	
+	tlm::tlm_command cmd_s=trans.get_command();
+	uint32 addr_s=trans.get_address();
+	unsigned char* data = trans.get_data_ptr();//<-handout miss "()", CHAR* BY DEF
+	unsigned int len = trans.get_data_length();
+	if (cmd_s == tlm::TLM_WRITE_COMMAND){//write op
+		data_ptr=(reinterpret_cast<unsigned int*>(data));
+		data=*(data_ptr);
+		offset=addrs-BASE;
+	}
+	//else{//read op
+		
+	//}
+
+}
+void DMA::dma_p(){
+	
+	while(1){//main dma process
+		wait();
+		tlm::tlm_generic_payload* trans_m = new tlm::tlm_generic_payload;
+		sc_time DELAY = sc_time(10,SC_NS);
+		if(Interrupt==1){
+			if(offset==12){
+				START_CLEAR=data;
+			}
+			if(START_CLEAR==0){
+				Interrupt.write(0);
+			}
+		}
+		if(START_CLEAR==0){
+			switch(offset){
+				case 0:
+					SOURCE=data;
+					printf("Write SOURCE:%#010x\n",SOURCE);
+					break;
+				case 4:
+					TARGET=data;
+					printf("Write TARGET:%#010x\n",TARGET);
+					break;
+				case 8:
+					SIZE=data;
+					printf("Write SIZE:%d\n",SIZE);
+					break;
+				case 12:
+					START_CLEAR=data;
+					printf("Write START\n");
+					break;
+			}
+		}//if end
+		if(START_CLEAR==1&&SIZE>0){
+			printf("DMA start to work, moving data from %#010x to %#010x, size %d\n",SOURCE,TARGET,SIZE);
+			
+			int size_iter=SIZE;
+			int addr_s=SOURCE;
+			int addr_t=TARGET;
+			while(size_iter!=){//moving data
+				tlm::tlm_command cmd_mr;
+				cmd_mr = tlm::TLM_READ_COMMAND;
+				trans_m->set_command(cmd_mr);
+				trans_m->set_address(addr_s);
+				trans_m->set_data_ptr(reinterpret_cast<unsigned char*>(&data_m));
+				trans_m->set_data_length(4);
+				trans_m->set_streaming_width(4);
+				trans_m->set_byte_enable_ptr(0);
+				trans_m->set_dmi_allowed(false);
+				trans_m->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+				socket_m->b_transport( *trans_m, delay );
+				
+				printf("DMA read from source\n");
+				
+				tlm::tlm_command cmd_mw;
+				cmd_mw=tlm::TLM_WRITE_COMMAND;
+				//mask data
+				if(size_iter==1) data_m=data_m&0x000000FF;
+				else if(size_iter==2) data_m=data_m&0x0000FFFF;
+				else if(size_iter==3) data_m=data_m&0x00FFFFFF;
+				
+				printf("DMA write target\n");
+				
+				trans_m->set_command(cmd_mw);
+				trans_m->set_address(addr_t);
+				trans_m->set_data_ptr(reinterpret_cast<unsigned char*>(&data_m));
+				trans_m->set_data_length(4);
+				trans_m->set_streaming_width(4);
+				trans_m->set_byte_enable_ptr(0);
+				trans_m->set_dmi_allowed(false);
+				trans_m->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+				socket_m->b_transport(*trans_m,delay);
+				
+				//iter
+				size_iter-=4;
+				addr_s+=4;
+				addr_t+=4;
+			}//moving data loop end
+			
+			Interrupt.write(1);
+		}
+
+	}//process end
+}
---
+++ HW2/DMA.h
@@ -0,0 +1,55 @@
+#ifndef DMA_H
+#define DMA_H
+
+#include <stdio.h>
+#include <iostream>
+
+#include "tlm.h"
+#include "tlm_utils/simple_target_socket.h"
+#include "tlm_utils/simple_initiator_socket.h"
+using namespace sc_core;
+using namespace sc_dt;
+using namespace std;
+using namespace tlm_utils;
+
+
+SC_MODULE(DMA){
+	//public
+	sc_in<bool> reset;
+	sc_out<bool> Interrupt;
+	
+	simple_target_socket<DMA> master_p;
+	simple_initiator_socket<DMA> slave_p;
+	sc_uint<32> SOURCE,
+				TARGET,
+				SIZE;
+	bool 		START_CLEAR;
+	
+	//protect
+	sc_signal<sc_uint<32> > BASE;
+	int offset,
+	unsigned int data,data_m;
+	unsigned int* data_ptr;
+	
+	
+	void dma_p();
+	void b_transport(tlm::tlm_generic_payload&, sc_time&);
+	
+	SC_CTOR(DMA):master_p(“master_p”), slave_p(“slave_p”){
+		slave_p.register_b_transport(this, &DMA::b_transport);
+		SC_CTHREAD(dmp_p, clk.pos());
+		reset_signal_is(reset,true);
+		BASE=0xA0000000;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+#endif
